a1373e720aa0e828f2147da276470ed178ea5ae6
Nino Grigalashvili <nini.grigalashvili@gmail.com>
2020-12-01 15:48:15 +0400

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './store/configureStore';
 
 
-const store = configureStore();
+const {store, persistor} = configureStore();
 ReactDOM.render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
   ,
   document.getElementById('root')
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,18 +2,35 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import VideosReducer from './videos/videos.reducer';
 import FavouritesReducer from './favourites/favourites.reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const favouritesPersistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['FavouritesReducer'] // only VideosReducer will be persisted
+};
+
+const rootReducer = combineReducers({
+    VideosReducer,
+    FavouritesReducer
+})
+
+const persistedReducer = persistReducer(favouritesPersistConfig, rootReducer)
 
 const componentEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 //Store creation
 const configureStore = () => {
     const store = createStore(
-        combineReducers({
-             VideosReducer,
-             FavouritesReducer
-        }),
+        persistedReducer,
         componentEnhancers(applyMiddleware(thunk))
     );
-    return store;
+    const persistor = persistStore(store)
+
+    return { store, persistor };
 }
+
 
 export default configureStore;
